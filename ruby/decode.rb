224318b68795ea8e7b42cb9a074d0713da30f55c
Isigba Ambrose <ambrisexyz@gmail.com>
2022-09-28 14:14:41 +0200

    added methods

---
+++ ruby/decode.rb
@@ -0,0 +1,22 @@
+def decode_char(char)
+  my_obj_morse = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  my_obj_morse[char]
+end
+
+def decode_word(word)
+  my_string = ''
+  word.split.each do |char|
+    my_string += decode_char(char)
+  end
+  my_string
+end
+
+puts decode_char('..-')
+puts decode_word('-.-. --- -- .')
